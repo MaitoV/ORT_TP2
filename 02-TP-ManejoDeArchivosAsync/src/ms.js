e42192d464ca0602c4b5ce2440b54a0ac23e7ab3
Maito <maite.v@outlook.com>
2024-04-23 15:44:03 -0300

    lectura y esritura sincronica y asincronica con callbacks

---
+++ 02-TP-ManejoDeArchivosAsync/src/ms.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+try {
+    let rutaArchivoLectura = path.join(__dirname,'..','package.json');
+    let rutaArchivoEscritura = path.join(__dirname,'..', 'info.txt');
+
+    let archivoString = fs.readFileSync(rutaArchivoLectura, 'utf-8');
+    let archivoObjeto = JSON.parse(archivoString);
+    let tamanioArchivo = fs.statSync(rutaArchivoLectura).size;
+
+    let info = {
+        contenidoStr: archivoString,
+        contenidoObj: archivoObjeto,
+        size: tamanioArchivo
+    }
+
+    let infoJSON = JSON.stringify(info);
+    fs.writeFileSync(rutaArchivoEscritura, infoJSON);
+
+} catch (error) {
+    console.log(`ERROR: ${error.message}`)
+}
